0cade8ee3e58e61aa5a3df60c24bd3a27089a774
Erin David <erindavid001@gmail.com>
2022-03-30 10:22:14 -0700

    fixed the js file

--- gulp-script/gulpfile.js
+++ gulp-script/gulpfile.js
@@ -2,7 +2,7 @@ const { src, dest, watch, series, parallel } = require('gulp');
 
 function helloGulp (done){
     console.log ('hello, gulp world!');
-    cb();
+    done();
 }
 
 exports.hello = helloGulp;
